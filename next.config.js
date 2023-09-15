110b4e9bf434e094663820a1e1dd1af1cd7ce58d
warlockja <warlockja@gmail.com>
2023-09-15 11:32:58 +0300

    Added Discord provider

--- next.config.js
+++ next.config.js
@@ -3,22 +3,32 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        // github
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
         port: "",
         pathname: "/u/**",
       },
       {
+        // facebook
         protocol: "https",
         hostname: "scontent.*.fna.fbcdn.net",
         port: "",
         pathname: "/v/**",
       },
       {
+        // google
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
         port: "",
         pathname: "/a/**",
+      },
+      {
+        // discord
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/avatars/**",
       },
     ],
   },
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 // import FacebookProvider from "next-auth/providers/facebook";
+import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
@@ -45,6 +46,10 @@ export const options: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
